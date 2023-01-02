2f2b5872a93699792dd837a8ed492cbc961da0b0
layla <cisneroslay@hotmail.com>
2023-01-02 10:21:34 -0300

    initial commit

---
+++ gitTest/gitTest/gitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitTestApp.swift
+//  gitTest
+//
+//  Created by Layla Cisneros on 02/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct gitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
